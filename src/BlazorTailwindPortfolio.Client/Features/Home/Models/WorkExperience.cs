d3ba661f6e84014266023ceb844b4f661b2ccda6
wilsonsouto <wilsonsouto@outlook.com.br>
2026-03-10 21:20:05 -0300

    chore: update aria label and work experience model

--- src/BlazorTailwindPortfolio.Client/Features/Home/Models/WorkExperience.cs
+++ src/BlazorTailwindPortfolio.Client/Features/Home/Models/WorkExperience.cs
@@ -3,7 +3,4 @@
     public record WorkExperience(Company Company, string Role, Duration Duration, String[] Responsabilities, String[] Stacks, DisplayOptions DisplayOptions);
 
     public record Company(string Name, string Url);
-    
-    public record Duration(string ShortDate, string LongDate);
 }
- 

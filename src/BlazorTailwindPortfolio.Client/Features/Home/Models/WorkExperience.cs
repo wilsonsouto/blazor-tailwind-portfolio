ee8178c058a93dc955dea8c7ebf44093b1779de9
wilsonsouto <wilsonsouto@outlook.com.br>
2026-03-11 21:47:51 -0300

    chore: rename stack parameter and add new stack

--- src/BlazorTailwindPortfolio.Client/Features/Home/Models/WorkExperience.cs
+++ src/BlazorTailwindPortfolio.Client/Features/Home/Models/WorkExperience.cs
@@ -1,6 +1,6 @@
 ﻿namespace BlazorTailwindPortfolio.Client.Features.Home.Models
 {
-    public record WorkExperience(Company Company, string Role, Duration Duration, String[] Responsabilities, String[] Stacks, DisplayOptions DisplayOptions);
+    public record WorkExperience(Company Company, string Role, Duration Duration, String[] Responsabilities, String[] Stack, DisplayOptions DisplayOptions);
 
     public record Company(string Name, string Url);
 }
